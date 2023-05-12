af4f36bf9156fa8b2cd782bd560317e301d702b9
Yuanze <2292175116@qq.com>
2023-05-13 01:11:24 +0800

    session实现登录状态保持
    且每次访问都会更新时间

--- Odap/src/main/java/com/example/odap/controller/LoginController.java
+++ Odap/src/main/java/com/example/odap/controller/LoginController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,7 +28,7 @@ public class LoginController {
     private static final String MSG_FAILURE = "Login failed.";
 
     @PostMapping("/login")
-    public ResponseEntity<Map<String, Object>> login(@RequestBody LoginForm loginForm) {
+    public ResponseEntity<Map<String, Object>> login(HttpServletRequest request, @RequestBody LoginForm loginForm) {
         Map<String, Object> response = new HashMap<>();
         User user = userService.findUserByName(loginForm.getUsername());
         if (user == null || !user.getPassWord().equals(loginForm.getPassword())) {
@@ -35,6 +37,13 @@ public class LoginController {
             response.put("data", null);
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
         }
+
+        // 将用户信息保存到会话，并设置会话的超时时间为10分钟
+        HttpSession session = request.getSession();
+        session.setAttribute("user", user);
+        session.setMaxInactiveInterval(10 * 60); // 10分钟
+
+
         response.put("code", CODE_SUCCESS);
         response.put("error_msg", MSG_SUCCESS);
         Map<String, Object> data = new HashMap<>();
